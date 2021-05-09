5e3a2f6deba03cdbaf760dcccfa0c1a3cdd7a5a5
16478 <16478@DESKTOP-E0RAJC4>
2021-05-09 00:41:48 -0400

    1st commit

---
+++ Music_player_3/src/main/MP3.java
@@ -0,0 +1,20 @@
+package main;
+
+/**
+ *
+ * @author 16478
+ */
+public class MP3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        mplayer mplayer = new mplayer();
+        mplayer.setVisible(true);
+        mplayer.setResizable(false);
+        mplayer.setLocationRelativeTo(null);
+    }
+    
+}
